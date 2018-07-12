474cb27069fa0574c6816ad6f55cd94256c9d513
Guilherme Coelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2018-07-11 22:49:19 -0300

    Algumas mudancas apos a revisao

--- src/util/CategoriaEnum.java
+++ src/util/CategoriaEnum.java
@@ -1,40 +1,39 @@
 package util;
 
-/** 
- * Classe responsável por "enumerar" a categoria, retornando o tipo
- * da categoria do Item compravel.
+/**
+ * Classe responsável por "enumerar" a categoria, retornando o tipo da categoria
+ * do Item compravel.
  * 
  * @author José Guilheme - Matricula: 117210370
- * @author Mariana Nascimento - Matricula: 117210395
- * @author Siuanny Barbosa - Matriucla: 
- * @author Thayanne Sousa - Matricula:
+ * @author Mariana Nascimento - Matricula: 117210416
+ * @author Siuanny Barbosa - Matriucla: 117210395
+ * @author Thayanne Sousa - Matricula: 117210414
  * UFCG/2018.1 - Laboratório de Programação 2 - Projeto de Laboratorio (Lista pra mim)
  */
 public enum CategoriaEnum {
 
-	ALIMENTOS_INDUSTRIALIZADOS("alimento industrializado"), 
-	ALIMENTOS_NAO_INDUSTRIALIZADOS("alimento nao industrializado"), 
-	LIMPEZA("limpeza"),
+	ALIMENTOS_INDUSTRIALIZADOS("alimento industrializado"),
+	ALIMENTOS_NAO_INDUSTRIALIZADOS("alimento nao industrializado"), LIMPEZA("limpeza"),
 	HIGIENE_PESSOAL("higiene pessoal");
-	
+
 	private String descricao;
-	
+
 	/**
 	 * Inicializa a categoria com a sua descricao
-	 * @param descricao
-	 * 			tipo da categoria
+	 * 
+	 * @param descricao tipo da categoria
 	 */
 	private CategoriaEnum(String descricao) {
 		this.descricao = descricao;
 	}
 
-	
 	/**
 	 * Retorna a categoria do item compravel.
+	 * 
 	 * @return String - representando a categoria
 	 */
 	@Override
 	public String toString() {
-		return descricao;
+		return this.descricao;
 	}
 }

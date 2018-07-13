31e7ec029ec71ccc5fd0c46a8db3c804e659bc4e
Siuanny Barbosa <siuanny.rocha@ccc.ufcg.edu.br>
2018-07-13 20:55:54 -0300

    Adicionado teste para toString seguindo exemplo passado na especificacao

--- src/test/ItemPorQtdTest.java
+++ src/test/ItemPorQtdTest.java
@@ -9,12 +9,14 @@ public class ItemPorQtdTest {
 
 	private ItemPorQtd item;
 	private ItemPorQtd sabonete;
-	
+
 	@Before
 	public void init() {
 		sabonete = new ItemPorQtd("Sabonete de aveia Dorene", 13255, "higiene pessoal", 5, "gramas", "BaratoD+", 15.00);
+		algodao = new ItemPorQtd("Algodão Clemer", 219, "higiene pessoal", 300, "gramas", "Supermercado BaratoD+", 2.33);
+		algodao.adicionaPreco("Baratao", 2.30);
 	}
-	
+
 	@Test
 	public void testConstrutor() {
 		assertNull(item);
@@ -36,17 +38,17 @@ public class ItemPorQtdTest {
 	public void testConstrutorIDNegativo() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", -13255, "higiene pessoal", 5, "gramas", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorIDZero() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 0, "higiene pessoal", 5, "gramas", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorCategoriaVazia() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "", 5, "gramas", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorCategoriaNula() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, null, 5, "gramas", "BaratoD+", 15.00);
@@ -56,7 +58,7 @@ public class ItemPorQtdTest {
 	public void testConstrutorQuantidadeNegativa() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", -5, "gramas", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorQuantidadeZero() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 0, "gramas", "BaratoD+", 15.00);
@@ -66,12 +68,12 @@ public class ItemPorQtdTest {
 	public void testConstrutorUnidadeDeMedidaVazia() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorUnidadeDeMedidaApenasEspacos() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "      ", "BaratoD+", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorUnidadeDeMedidaNula() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, null, "BaratoD+", 15.00);
@@ -81,22 +83,22 @@ public class ItemPorQtdTest {
 	public void testConstrutorLocalVazio() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "gramas", "", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorLocalApenasEspacos() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "gramas", "            ", 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorLocalNull() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "gramas", null, 15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorValorNegativo() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "gramas", "BaratoD+", -15.00);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testConstrutorValorZero() {
 		item = new ItemPorQtd("Sabonete de aveia Dorene", 1254884, "higiene pessoal", 5, "gramas", "BaratoD+", 0);
@@ -123,7 +125,7 @@ public class ItemPorQtdTest {
 	public void testSetQuantidadeNegativa() {
 		sabonete.setQuantidade(-10);
 	}
-	
+
 	@Test(expected=IllegalArgumentException.class)
 	public void testSetQuantidadeZero() {
 		sabonete.setQuantidade(0);
@@ -145,10 +147,15 @@ public class ItemPorQtdTest {
 	public void testSetUnidadeDeMedidaApenasEspacos() {
 		sabonete.setUnidade("             ");
 	}
-	
+
 	@Test(expected=RuntimeException.class)
 	public void testSetUnidadeDeMedidaNula() {
 		sabonete.setUnidade(null);
 	}
-	
+
+	@Test
+	public void testToString() {
+		assertEquals("219. Algodão Clemer, higiene pessoal, 300 gramas, Preco: <Supermercado BaratoD+, R$ 2,33;  Baratão, R$ 2,30>", abonete.toString());
+	}
+
 }
